32263656b25362526e4eb56e6f53bb2261479315
patricksu168 <0ocookiezio0@gmail.com>
2018-11-21 14:09:46 +1100

    minor bug fix
    fix increment base_ack when clearing buffer at line 60

--- receiver.py
+++ receiver.py
@@ -24,67 +24,69 @@ class STPReceiver:
         serverSocket.bind(('', self.port_num))
         print("The server is ready to receive")
 
-        while (1):
+        while 1:
             #receive segment
             seg_string, address = serverSocket.recvfrom(2048)
             segment = json.loads(seg_string.decode('utf-8'))
             print(segment)
             print(self.base_ACK)
-            if (segment["SYN"] == 1):
+            if segment["SYN"] == 1:
                 #send SYNACK segment back to sender
                 self.base_ACK = segment["seq_num"] + 1
                 self.send(serverSocket, segment)
                 self.init_seq_num += 1
 
             #end of 3 way handshake where client acknowledge SYNACK segment
-            elif (segment["FIN"] == 0 and  segment["data"] == ""):
+            elif segment["FIN"] == 0 and  segment["data"] == "":
                 self.base_ACK += 1
-
             elif (segment["data"] != ""):
                 if not self.file:
 
                     #create the text file to be written
                     self.file = open(self.file_name, "w+")
 
-                if (segment["seq_num"] == self.base_ACK):
+                if segment["seq_num"] == self.base_ACK:
                     #if sequence number of the segment is what the server is expecting, simply
                     #send the segment to upper level by extracting the data
                     self.extract(segment)
                     self.base_ACK += len(segment["data"])
                     #if there are segments buffered, that means the gap is completely filled and extract
                     #all segments buffered
-                    if (self.buffer != []):
-                        if (self.buffer[0].seq_num == self.base_ACK):
-                            for seg in self.buffer:
+                    if self.buffer != []:
+                        for seg in self.buffer:
+                            if seg["seq_num"] == self.base_ACK:
+
                                 self.extract(seg)
-                                self.base_ACK += len(seg.data)
+                                self.base_ACK += len(seg["data"])
 
                     self.send(serverSocket, segment)
                 #there is gap detected between server's expected next sequence number and that of received segment
                 #therefore buffer the segment until the gap is filled
-                else:
+                elif segment["seq_num"] > self.base_ACK:
                     self.buffer.append(segment)
                     self.send(serverSocket, segment)
-            elif (segment["FIN"] == 1):
-                self.file.close()
-                self.base_ACK += 1
-                self.send(serverSocket, segment)
-                self.send(serverSocket, segment)
-                segment, address = serverSocket.recvfrom(2048)
-                break
+            elif segment["FIN"] == 1:
+                if segment["seq_num"] == self.base_ACK:
+                    self.base_ACK += 1
+                    self.send(serverSocket, segment)
+                    self.send(serverSocket, segment)
+                    segment, address = serverSocket.recvfrom(2048)
+                    break
+                else:
+                    segment["FIN"] = 0
+                    self.send(serverSocket, segment)
+
 
         #time.sleep(10)
+        self.file.close()
         print("The server is closing down")
         serverSocket.close()
 
     #send ack packet to sender through serverSocket
     def send(self, serverSocket, segment):
-        increment = 0
 
         #if segment has no data, it's either a SYN segment or a FIN segment,
         #therefore need to increment acknowledgement number as well as sequence number of the next packet by 1
-        if segment["data"] != "":
-            increment = 1
 
         seg_string = {
             "source_ip": self.ip,
@@ -92,7 +94,7 @@ class STPReceiver:
             "data": "",
             "SYN": segment["SYN"],
             "FIN": segment["FIN"],
-            "ACK_num": self.base_ACK + increment,
+            "ACK_num": self.base_ACK,
             "seq_num": self.init_seq_num
         }
         ack_seg = json.dumps(seg_string)
